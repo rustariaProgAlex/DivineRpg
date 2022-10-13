7acc3c091f711154e1faba3acbf5113db48024a4
ffoDevilSuseJ <86656641+ffoDevilSusiJ@users.noreply.github.com>
2022-10-13 13:35:50 +0300

    new file:   plot/locations.txt 	modified:   src/main/java/com/dev/Main.java 	modified:   src/main/java/com/files/FileListener.java 	modified:   target/classes/com/dev/Main.class 	modified:   target/classes/com/files/FileListener.class

--- src/main/java/com/dev/Main.java
+++ src/main/java/com/dev/Main.java
@@ -1,13 +1,20 @@
 package com.dev;
 
+import java.io.File;
+
+import com.files.FileListener;
 import com.location.Location;
 
 import javafx.scene.control.Button;
 
 public class Main {
-    Location currentLocation;
+    static Location currentLocation;
     public static void travel(){
 
+    }
+    public static void locationHandler(){
+        Location location = FileListener.nashmethod();
+        currentLocation = location;
     } 
     
     
--- src/main/java/com/files/FileListener.java
+++ src/main/java/com/files/FileListener.java
@@ -6,31 +6,39 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import com.location.Location;
+
 public class FileListener {
     public static String textReading(String id, String path) {
         StringBuilder sb = null;
-        try {     
+        try {
             BufferedReader reader = Files.newBufferedReader(Path.of(path), StandardCharsets.UTF_8);
-             sb = new StringBuilder();
+            sb = new StringBuilder();
             String line = reader.readLine();
             boolean pass = false;
             while (line != null) {
-                if (line.contains("$") && line.contains(id)){
-                   pass = true;
+                if (line.contains("$") && line.contains(id)) {
+                    pass = true;
                 }
-                if (pass && !line.contains("$") ){   
+                if (pass && !line.contains("$")) {
                     sb.append(line);
                 }
                 line = reader.readLine();
-                if (line != null && line.contains("$") && pass){
+                if (line != null && line.contains("$") && pass) {
                     break;
                 }
-                
+
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
+
         return sb.toString().replace("null", "");
+    }
+
+    public static Location locationReader(String path, String id) {
+
+        
+        return null;
     }
 }
