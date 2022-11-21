b3a3f1abb3bcf9e2c781c181baf7456e91d8f2c4
rustariaProgAlex <114904489+rustariaProgAlex@users.noreply.github.com>
2022-11-21 13:25:50 +0300

    modified:   src/main/java/com/dev/Main.java 	new file:   target/classes/com/dev/Main$1.class 	new file:   target/classes/com/dev/Main$2.class 	new file:   target/classes/com/dev/Main$Fofa.class 	modified:   target/classes/com/dev/Main.class

--- src/main/java/com/dev/Main.java
+++ src/main/java/com/dev/Main.java
@@ -6,23 +6,51 @@ import com.files.FileListener;
 import com.hero.Player;
 import com.location.Location;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 
 public class Main {
     static Location currentLocation;
     public static void travel(){
         currentLocation = FileListener.locationReader("plot/locations.txt", "LocationForest_1");
-        while (Player.getHp() <= 0) {
-            for(int i = 0; i < currentLocation.getDist(); i++){
+        new Thread(new Runnable() {
 
+            @Override
+            public void run() {
+                while (Player.getHp() <= 0) {
+                    for(int i = 0; i < currentLocation.getDist(); i++){
+        
+                    }
+                    
+                }
+                
             }
             
-        }
+        }).start();
 
     }
     public static void locationHandler(){
     } 
     
-    private void 
-    
+    public static void showTravelMenu(){
+        Button test = new Button("1");
+        App.getBtnBox().getChildren().clear();
+        App.getBtnBox().getChildren().add(test);
+        test.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent arg0) {
+                // TODO Auto-generated method stub
+                
+            }
+            
+        });
+    }
+
+
+    class Fofa implements EventHandler<ActionEvent>
+    {
+        
+    } 
 }
